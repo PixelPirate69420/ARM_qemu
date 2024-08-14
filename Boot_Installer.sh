1255fbef73d072b3570c24dea23316026dbf5ffa
PixelGoose <131998579+PixelPirate69420@users.noreply.github.com>
2024-08-14 09:46:20 -0400

    Create Boot_Installer.sh
    With this template you have to rename your ISO to cdrom.iso and it's meant as a template for the first start of the VM, just to install the OS

---
+++ Boot_Installer.sh
@@ -0,0 +1,13 @@
+qemu-system-aarch64 \
+  -m 4096 \
+  -smp 4 \
+  -cpu cortex-a57 \
+  -machine type=virt-3.0 \
+  -drive file=disk.img,if=none,id=drive0,format=raw \
+  -device virtio-blk-device,drive=drive0 \
+  -cdrom cdrom.iso \
+  -boot d \
+  -netdev user,id=net0 \
+  -device virtio-net-device,netdev=net0 \
+  -device virtio-gpu-pci \
+  -bios AAVMF_CODE.fd
