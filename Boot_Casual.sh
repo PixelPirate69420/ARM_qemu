87a02d85e3645c46435836646901ab09a579444e
PixelGoose <131998579+PixelPirate69420@users.noreply.github.com>
2024-08-14 09:53:30 -0400

    Create Boot_Casual.sh
    This template is meant for booting casually from disk.img

---
+++ Boot_Casual.sh
@@ -0,0 +1,11 @@
+qemu-system-aarch64 \
+  -m 4096 \
+  -smp 4 \
+  -cpu cortex-a57 \
+  -machine type=virt-3.0 \
+  -drive file=disk.img,if=none,id=drive0,format=raw \
+  -device virtio-blk-device,drive=drive0 \
+  -netdev user,id=net0 \
+  -device virtio-net-device,netdev=net0 \
+  -device vmware-svga \
+  -bios AAVMF_CODE.fd
